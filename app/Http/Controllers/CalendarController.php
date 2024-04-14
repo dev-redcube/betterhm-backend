490640e1e0fd877bbb7c97bb747054d404924380
Moritz <2nd@moritzhuber.de>
2024-04-14 23:18:32 +0200

    rename

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -11,7 +11,7 @@ class CalendarController extends Controller
     {
         $fristenTermine = new Calendar();
         $fristenTermine->id = "1776ed34-fccf-4845-aa1b-5c6821649804";
-        $fristenTermine->name = "Fakultät 10 Fristen & Termine";
+        $fristenTermine->name = "Fristen & Termine";
         $fristenTermine->url = "https://calendar.google.com/calendar/ical/qp1nk83jvkfm7a3m9goqhr5oo4%40group.calendar.google.com/public/basic.ics";
 
         $thunderbird = new Calendar();
